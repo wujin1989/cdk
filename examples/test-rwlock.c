23bc3762604426f7c72d278d75fd0e91f3c81431
Jin.Wu <wujin.developer@gmail.com>
2023-03-19 12:31:28 +0800

    update

--- examples/test-rwlock.c
+++ examples/test-rwlock.c
@@ -7,7 +7,7 @@ int reader(void* p) {
 
 	while (true) {
 		cdk_rwlock_rdlock(&rwlock);
-		printf("[%d] read cnt: %d\n", (int)cdk_utils_systemtid(), cnt);
+		cdk_logi("[%d] read cnt: %d\n", (int)cdk_utils_systemtid(), cnt);
 		cdk_rwlock_unlock(&rwlock);
 
 		cdk_time_sleep(2000);
@@ -19,7 +19,7 @@ int writer(void* p) {
 
 	while (true) {
 		cdk_rwlock_wrlock(&rwlock);
-		printf("[%d] write cnt: %d\n", (int)cdk_utils_systemtid(), ++cnt);
+		cdk_logi("[%d] write cnt: %d\n", (int)cdk_utils_systemtid(), ++cnt);
 		cdk_rwlock_unlock(&rwlock);
 
 		cdk_time_sleep(4000);
@@ -29,6 +29,7 @@ int writer(void* p) {
 }
 int main(void) {
 
+	cdk_logger_create(NULL, 0);
 	cdk_rwlock_init(&rwlock);
 
 	for (int i = 0; i < 3; i++) {
@@ -40,5 +41,6 @@ int main(void) {
 	thrd_create(&t2, writer, NULL);
 
 	thrd_join(t2, NULL);
+	cdk_logger_destroy();
 	return 0;
 }
