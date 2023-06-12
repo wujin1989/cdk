01d0b9b7748f901e00b4be257a84c5ed7346d62e
Wu Jin <wujin.developer@gmail.com>
2023-06-12 09:14:42 +0800

    update

--- examples/example-tcpserver.c
+++ examples/example-tcpserver.c
@@ -73,7 +73,7 @@ int main(void) {
 		.keyfile = "certs/server.key",
 		.verifypeer = false
 	};
-    cdk_net_startup(4, /*NULL*/&conf);
+    cdk_net_startup(4, NULL/*&conf*/);
 
 	cdk_handler_t handler = {
 		.on_accept  = handle_accept,
