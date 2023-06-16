2c075fcb91ee972d40945a65331d436bc5c31377
Jin.Wu <wujin.developer@gmail.com>
2023-06-16 22:26:56 +0800

    update

--- examples/example-logger.c
+++ examples/example-logger.c
@@ -1,37 +1,9 @@
 #include "cdk.h"
 
-int i;
-mtx_t mtx;
-
-int routine(void* param) {
-
-	while (true) {
-		mtx_lock(&mtx);
-		++i;
-		cdk_logi("sub thread output: %d\n", i);
-		mtx_unlock(&mtx);
-
-		cdk_time_sleep(100);
-	}
-	return 0;
-}
-
 int main(void) 
 {
 	cdk_logger_create(NULL, 4);
-	thrd_t tid;
-	mtx_init(&mtx, mtx_plain);
-	thrd_create(&tid, routine, NULL);
-	thrd_detach(tid);
-	
-	while (1) {
-		mtx_lock(&mtx);
-		++i;
-		cdk_logi("main thread output: %d\n", i);
-		mtx_unlock(&mtx);
-		cdk_time_sleep(100);
-	}
-	mtx_destroy(&mtx);
+	cdk_logi("logging\n");
 	cdk_logger_destroy();
 	return 0;
 }
