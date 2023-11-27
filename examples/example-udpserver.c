5248f6abed8153d2c665b4bc87b687fb26e7cf4d
Wu Jin <wujin.developer@gmail.com>
2023-11-27 17:29:44 +0800

    update

--- examples/example-udpserver.c
+++ examples/example-udpserver.c
@@ -1,29 +1,65 @@
 #include "cdk.h"
 
+typedef struct synchronized_queue_s {
+	cdk_queue_t data;
+	mtx_t lock;
+	cnd_t cond;
+}synchronized_queue_t;
+
+typedef cdk_queue_node_t synchronized_queue_node_t;
+
+#define synchronized_queue_data cdk_list_data
+
+static synchronized_queue_t mq;
+
 typedef struct msg_s {
 	cdk_channel_t* conn;
-	cdk_queue_node_t node;
+	synchronized_queue_node_t node;
 	size_t len;
 	char   buf[];
 }msg_t;
 
-cdk_queue_t mq;
+void synchronized_queue_create(synchronized_queue_t* queue) {
+	cdk_queue_init(&queue->data);
+	mtx_init(&queue->lock, mtx_plain);
+	cnd_init(&queue->cond);
+}
+
+void synchronized_queue_enqueue(synchronized_queue_t* queue, synchronized_queue_node_t* node) {
+	mtx_lock(&queue->lock);
+	cdk_queue_enqueue(&queue->data, node);
+	cnd_signal(&queue->cond);
+	mtx_unlock(&queue->lock);
+}
+
+bool synchronized_queue_empty(synchronized_queue_t* queue) {
+	return cdk_queue_empty(&queue->data);
+}
+
+synchronized_queue_node_t* synchronized_queue_dequeue(synchronized_queue_t* queue) {
+	mtx_lock(&queue->lock);
+	while (synchronized_queue_empty(queue)) {
+		cnd_wait(&queue->cond, &queue->lock);
+	}
+	cdk_queue_node_t* node = cdk_queue_dequeue(&queue->data);
+	mtx_unlock(&queue->lock);
+	return node;
+}
+
+void synchronized_queue_destroy(synchronized_queue_t* queue) {
+	mtx_destroy(&queue->lock);
+	cnd_destroy(&queue->cond);
+}
 
 int routine(void* p) {
-	
 	while (true) {
-		if (!cdk_queue_empty(&mq)) {
-			msg_t* msg = cdk_queue_data(cdk_queue_dequeue(&mq), msg_t, node);
-			printf("received buf: %s, len: %zu\n", msg->buf, msg->len);
+		msg_t* msg = synchronized_queue_data(synchronized_queue_dequeue(&mq), msg_t, node);
+		printf("received buf: %s, len: %zu\n", msg->buf, msg->len);
 
-			cdk_net_postsend(msg->conn, "recvive complete.", strlen("recvive complete.") + 1);
+		cdk_net_postsend(msg->conn, "recvive complete.", strlen("recvive complete.") + 1);
 
-			free(msg);
-			msg = NULL;
-		}
-		else {
-			cdk_time_sleep(1000);
-		}
+		free(msg);
+		msg = NULL;
 	}
 	return 0;
 }
@@ -32,17 +68,17 @@ static void handle_write(cdk_channel_t* conn, void* buf, size_t len) {
 	cdk_net_postrecv(conn);
 }
 static void handle_read(cdk_channel_t* conn, void* buf, size_t len) {
-
 	cdk_addrinfo_t ai;
 	cdk_net_ntop(&conn->udp.peer.ss, &ai);
 
 	msg_t* msg = malloc(sizeof(msg_t) + len);
-	if (msg) {
-		msg->len = len;
-		msg->conn = conn;
-		memcpy(msg->buf, buf, len);
-		cdk_queue_enqueue(&mq, &msg->node);
+	if (!msg) {
+		return;
 	}
+	msg->len = len;
+	msg->conn = conn;
+	memcpy(msg->buf, buf, len);
+	synchronized_queue_enqueue(&mq, &msg->node);
 }
 
 static void handle_close(cdk_channel_t* conn, char* error) {
@@ -50,9 +86,8 @@ static void handle_close(cdk_channel_t* conn, char* error) {
 	cdk_net_close(conn);
 }
 int main(void) {
-
 	cdk_net_startup(4, NULL);
-	cdk_queue_init(&mq);
+	synchronized_queue_create(&mq);
 
 	thrd_t tid;
 	thrd_create(&tid, routine, NULL);
