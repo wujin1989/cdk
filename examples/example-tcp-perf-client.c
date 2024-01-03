ba4344675726b5341dc1b9c086418dbc901d94dc
Wu Jin <wujin.developer@gmail.com>
2024-01-03 11:13:52 +0800

    fix race with atomic type

--- examples/example-tcp-perf-client.c
+++ examples/example-tcp-perf-client.c
@@ -5,27 +5,27 @@
 
 cdk_timer_t timer;
 int total_clients = 100;
-int connected_clients;
-int disconnected_clients;
-int total_readcnt;
-size_t total_readbytes;
 char buffer[BUFFERSIZE];
+atomic_int connected_clients;
+atomic_int disconnected_clients;
+atomic_int total_readcnt;
+atomic_size_t total_readbytes;
 
 static void handle_connect(cdk_channel_t* channel) {
-	if (++connected_clients == total_clients) {
+	if (atomic_fetch_add(&connected_clients, 1) == total_clients) {
 		cdk_logi("%d clients has connected.\n", total_clients);
 	}
 	cdk_net_send(channel, buffer, sizeof(buffer));
 }
 
 static void handle_read(cdk_channel_t* channel, void* buf, size_t len) {
-	++total_readcnt;
-	total_readbytes += len;
+	atomic_fetch_add(&total_readcnt, 1);
+	atomic_fetch_add(&total_readbytes, len);
 	cdk_net_send(channel, buf, len);
 }
 
 static void handle_close(cdk_channel_t* channel, const char* error) {
-	if (++disconnected_clients == total_clients) {
+	if (atomic_fetch_add(&disconnected_clients, 1) == total_clients) {
 		cdk_logi("%d clients has disconnected.\n", total_clients);
 	}
 }
@@ -35,8 +35,8 @@ static inline void _test_finished(void* param) {
 }
 
 static void _printf_statistic_info() {
-	cdk_logi("tps:\t %d Q/s\n", total_readcnt / RUNTIME);
-	cdk_logi("throughput:\t %zu MB/s\n", (total_readbytes / (RUNTIME * 1024 * 1024)));
+	cdk_logi("tps:\t %d Q/s\n", atomic_load(&total_readcnt) / RUNTIME);
+	cdk_logi("throughput:\t %zu MB/s\n", (atomic_load(&total_readbytes) / (RUNTIME * 1024 * 1024)));
 }
 
 int main(void) {
@@ -53,6 +53,11 @@ int main(void) {
 		.tcp.on_close = handle_close,
 		.tcp.unpacker = &unpacker
 	};
+	atomic_init(&connected_clients, 0);
+	atomic_init(&disconnected_clients, 0);
+	atomic_init(&total_readcnt, 0);
+	atomic_init(&total_readbytes, 0);
+
 	for (int i = 0; i < total_clients; i++) {
 		cdk_net_dial("tcp", "127.0.0.1", "9999", &handler);
 	}
