4c9d1c35c8ade60aed69c18b151897183859430d
Wu Jin <wujin.developer@gmail.com>
2024-01-03 12:24:57 +0800

    update

--- examples/example-tcp-perf-client.c
+++ examples/example-tcp-perf-client.c
@@ -12,7 +12,8 @@ atomic_int total_readcnt;
 atomic_size_t total_readbytes;
 
 static void handle_connect(cdk_channel_t* channel) {
-	if (atomic_fetch_add(&connected_clients, 1) == total_clients) {
+	atomic_fetch_add(&connected_clients, 1);
+	if (atomic_load(&connected_clients) == total_clients) {
 		cdk_logi("%d clients has connected.\n", total_clients);
 	}
 	cdk_net_send(channel, buffer, sizeof(buffer));
@@ -25,7 +26,8 @@ static void handle_read(cdk_channel_t* channel, void* buf, size_t len) {
 }
 
 static void handle_close(cdk_channel_t* channel, const char* error) {
-	if (atomic_fetch_add(&disconnected_clients, 1) == total_clients) {
+	atomic_fetch_add(&disconnected_clients, 1);
+	if (atomic_load(&disconnected_clients) == total_clients) {
 		cdk_logi("%d clients has disconnected.\n", total_clients);
 	}
 }
@@ -53,10 +55,10 @@ int main(void) {
 		.tcp.on_close = handle_close,
 		.tcp.unpacker = &unpacker
 	};
-	atomic_init(&connected_clients, 1);
-	atomic_init(&disconnected_clients, 1);
-	atomic_init(&total_readcnt, 1);
-	atomic_init(&total_readbytes, 1);
+	atomic_init(&connected_clients, 0);
+	atomic_init(&disconnected_clients, 0);
+	atomic_init(&total_readcnt, 0);
+	atomic_init(&total_readbytes, 0);
 
 	for (int i = 0; i < total_clients; i++) {
 		cdk_net_dial("tcp", "127.0.0.1", "9999", &handler);
