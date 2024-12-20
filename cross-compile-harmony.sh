7550b2f6af705b4cf3172c872fca8e636a659fae
WuJin <n035029@archermind.com>
2024-12-20 16:05:28 +0800

    update

---
+++ cross-compile-harmony.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+OHOS_NDK_HOME=${HOME}/AppData/Local/OpenHarmony/Sdk/12/native
+export PATH=${OHOS_NDK_HOME}/build-tools/cmake/bin:$PATH
+set CMAKE_SYSTEM_NAME="Harmony"
+
+cmake.exe 																		\
+	-G Ninja 																	\
+	-B build 																	\
+	-DCMAKE_TOOLCHAIN_FILE="${OHOS_NDK_HOME}/build/cmake/ohos.toolchain.cmake" 	\
+	-DOHOS_ARCH=arm64-v8a														\
+	-DOHOS_STL=c++_static														\
+	
+cmake.exe --build build
